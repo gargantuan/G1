cfb7752adfcbb053ed8ad1b1ab115472f7de8cac
Thomas Elders <tomelders@Thomass-MacBook-Pro-2.local>
2015-05-26 17:12:37 +0100

    methods to combine two genomes

--- index.js
+++ index.js
@@ -3,4 +3,12 @@
 import Genome from './lib/Genome';
 
 var genome = new Genome();
-console.log(genome.compileGenes([0,1,15]));
+
+console.log('gene length', genome.calculateGeneSize([80,10,30,22]));
+var carAGenome = genome.encode([80,10,30,22]);
+var carBGenome = genome.encode([50,12,40,67]);
+var progeny = genome.transfer(carAGenome, carBGenome);
+
+console.log('carAGenome',carAGenome);
+console.log('carBGenome',carBGenome);
+console.log('progeny',progeny);
